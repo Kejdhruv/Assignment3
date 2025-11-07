83e742c7ceccf3517e5e96716a3996e2deb1378e
Dhruv <dhruvnkejriwal@gmail.com>
2025-11-08 01:01:12 +0530

    Complete DeBugging

--- frontend/src/Layout/Navbar.jsx
+++ frontend/src/Layout/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import {  useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import './Navbar.css'
 
 const Navbar = () => {
+  const navigate = useNavigate();
+     const handleLogout = () => {
+    fetch("http://localhost:8000/Auth/Logout", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then(() => navigate("/"))
+      .catch((err) => console.error("Logout failed", err));
+  };
+
   return (
     <nav className="home-navbar">
       <ul className="home-nav-links">
         <li><Link to="/Products">Products</Link></li>
         <li><Link to="/Profile">Profile</Link></li>
         <li><Link to="/Cart">Cart</Link></li>
-        <li><Link to="/">Login</Link></li>
+        <li onClick={handleLogout} ><Link  >Logout</Link></li>
       </ul>
     </nav>
   );
